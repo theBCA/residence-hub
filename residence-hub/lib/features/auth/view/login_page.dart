4def4cda4b322fcac7577a7375c66dce8b12231a
berk.arslan@kobil.com <berk.arslan@kobil.com>
2025-07-25 19:23:52 +0200

    Create GitHub Pages password reset in docs folder

--- residence-hub/lib/features/auth/view/login_page.dart
+++ residence-hub/lib/features/auth/view/login_page.dart
@@ -55,7 +55,7 @@ class _LoginPageState extends ConsumerState<LoginPage> {
     try {
       await Supabase.instance.client.auth.resetPasswordForEmail(
         _emailController.text,
-        redirectTo: 'https://thebca.github.io/residence-hub/residence-hub/reset-password.html',
+        redirectTo: 'https://thebca.github.io/residence-hub/',
       );
       
       if (mounted) {
