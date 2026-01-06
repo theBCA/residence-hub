f4a7c76df7e9278acf433e3149d59b18eadd0145
theBCA <berkcem123@gmail.com>
2026-01-06 09:58:27 +0100

    Change Supabase initialization parameters
    Updated Supabase initialization with empty URL and anonKey.

--- residence-hub/lib/main.dart
+++ residence-hub/lib/main.dart
@@ -167,8 +167,8 @@ class MyRootApp extends StatelessWidget {
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://vequxwdeoettksvaeybu.supabase.co',
-    anonKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlcXV4d2Rlb2V0dGtzdmFleWJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4Mjg5NjEsImV4cCI6MjA2ODQwNDk2MX0.tBOlg6XJ1ka_MCL7PCbst5JcJYdgKeFiu5mRHv1uIQg',
+    url: '',
+    anonKey:'',
   );
   runApp(
     const ProviderScope(
